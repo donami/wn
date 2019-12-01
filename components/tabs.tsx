c3acd3189913cb0a97917c22e6f761c8e71c9a71
Markus Hederstrom <d0namiz@gmail.com>
2019-12-01 11:07:06 +0100

    fix: update header

--- components/tabs.tsx
+++ components/tabs.tsx
@@ -4,6 +4,60 @@ import AllDrinksScreen from '../screens/all-drinks';
 import MyDrinksScreen from '../screens/my-drinks';
 import FavoritesScreen from '../screens/favorites';
 import { Icon } from 'react-native-elements';
+import { LinearGradient } from 'expo-linear-gradient';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import Text from './text';
+
+const TabBar = props => {
+  const {
+    renderIcon,
+    getLabelText,
+    activeTintColor,
+    inactiveTintColor,
+    onTabPress,
+    onTabLongPress,
+    getAccessibilityLabel,
+    navigation,
+  } = props;
+
+  const { routes, index: activeRouteIndex } = navigation.state;
+
+  return (
+    <LinearGradient
+      start={[0, 1]}
+      end={[1, 0]}
+      colors={['#F4C6A1', '#EEA18B', '#E77B77']}
+      style={styles.container}
+    >
+      {routes.map((route, routeIndex) => {
+        const isRouteActive = routeIndex === activeRouteIndex;
+        const tintColor = isRouteActive ? activeTintColor : inactiveTintColor;
+
+        return (
+          <TouchableOpacity
+            key={routeIndex}
+            style={
+              isRouteActive
+                ? [styles.tabButton, styles.tabButtonActive]
+                : styles.tabButton
+            }
+            onPress={() => {
+              onTabPress({ route });
+            }}
+            onLongPress={() => {
+              onTabLongPress({ route });
+            }}
+            accessibilityLabel={getAccessibilityLabel({ route })}
+          >
+            {renderIcon({ route, focused: isRouteActive, tintColor })}
+
+            <Text style={styles.text}>{getLabelText({ route })}</Text>
+          </TouchableOpacity>
+        );
+      })}
+    </LinearGradient>
+  );
+};
 
 const Tabs = createMaterialTopTabNavigator(
   {
@@ -55,11 +109,12 @@ const Tabs = createMaterialTopTabNavigator(
     },
   },
   {
+    tabBarComponent: TabBar,
     tabBarOptions: {
       showIcon: true,
       // showLabel: false,
       tabStyle: {
-        backgroundColor: '#333E43',
+        backgroundColor: 'transparent',
       },
       labelStyle: {
         fontFamily: 'Montserrat',
@@ -69,3 +124,19 @@ const Tabs = createMaterialTopTabNavigator(
 );
 
 export default Tabs;
+
+const styles = StyleSheet.create({
+  container: { flexDirection: 'row', height: 52, elevation: 2 },
+  tabButton: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomColor: 'transparent',
+    borderBottomWidth: 2,
+    borderStyle: 'solid',
+  },
+  tabButtonActive: {
+    borderBottomColor: '#97C7AC',
+  },
+  text: { color: 'white', textTransform: 'uppercase' },
+});
--- screens/header.tsx
+++ screens/header.tsx
@@ -1,48 +1,108 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
 import { SearchBar, Icon } from 'react-native-elements';
 import { useDispatch } from 'react-redux';
 import { searchAction } from '../redux/actions/search-actions';
 import { withNavigation } from 'react-navigation';
+import { LinearGradient } from 'expo-linear-gradient';
 
 type Props = {
   navigation?: any;
 };
 const HeaderScreen: React.FC<Props> = ({ navigation }) => {
   const [phrase, setPhrase] = useState('');
+  const [visible, setVisible] = useState(false);
   const dispatch = useDispatch();
 
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      start={[0, 1]}
+      end={[1, 0]}
+      colors={['#F4C6A1', '#EEA18B', '#E77B77']}
+      style={styles.container}
+    >
       <View style={styles.menuContainer}>
         <Icon
           name='bars'
           type='font-awesome'
-          color='#f50'
+          color='#fff'
           onPress={() => {
             navigation.openDrawer();
           }}
         />
       </View>
       <View style={styles.searchContainer}>
-        <SearchBar
-          containerStyle={styles.searchBarContainer}
-          platform={'default'}
-          style={styles.searchBarContainer}
-          lightTheme
-          onSubmitEditing={() => {
-            dispatch(searchAction(phrase));
-            navigation.navigate('Search', { phrase });
-          }}
-          // leftIconContainerStyle={styles.input}
-          // rightIconContainerStyle={styles.input}
-          // inputContainerStyle={styles.input}
-          placeholder='Type Here...'
-          onChangeText={value => setPhrase(value)}
-          value={phrase}
-        />
+        {!visible && (
+          <View style={{ alignSelf: 'flex-end', padding: 10, marginRight: 10 }}>
+            <TouchableOpacity onPress={() => setVisible(!visible)}>
+              <Icon name='search' color='#fff' />
+            </TouchableOpacity>
+          </View>
+        )}
+        {visible && (
+          <View
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              flex: 1,
+              padding: 20,
+              flexDirection: 'row',
+            }}
+          >
+            <View style={{ flex: 1 }}>
+              <SearchBar
+                containerStyle={styles.searchBarContainer}
+                platform={'default'}
+                style={styles.searchBarContainer}
+                lightTheme
+                onSubmitEditing={() => {
+                  dispatch(searchAction(phrase));
+                  navigation.navigate('Search', { phrase });
+                }}
+                searchIcon={props => (
+                  <Icon
+                    name='search'
+                    color='rgba(255, 255, 255, 0.7)'
+                    {...props}
+                  />
+                )}
+                clearIcon={props => (
+                  <Icon
+                    name='times'
+                    type='font-awesome'
+                    color='rgba(255, 255, 255, 0.7)'
+                    {...props}
+                  />
+                )}
+                // leftIconContainerStyle={styles.searchIcon}
+                // rightIconContainerStyle={styles.input}
+                inputContainerStyle={styles.input}
+                placeholder='Search drinks...'
+                placeholderTextColor='rgba(255, 255, 255, 0.7)'
+                inputStyle={{ color: 'rgba(255, 255, 255, 0.7)' }}
+                onChangeText={value => setPhrase(value)}
+                value={phrase}
+              />
+            </View>
+            <View style={{ flexGrow: 0, marginRight: 10 }}>
+              <TouchableOpacity
+                onPress={() => {
+                  setVisible(!visible);
+                  setPhrase('');
+                }}
+              >
+                <Icon
+                  name='chevron-right'
+                  type='font-awesome'
+                  color='rgba(255, 255, 255, 0.7)'
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
       </View>
-    </View>
+    </LinearGradient>
   );
 };
 
@@ -63,15 +123,14 @@ const styles = StyleSheet.create({
   },
   searchContainer: {
     flex: 2,
-    padding: 10,
   },
   searchBarContainer: {
     backgroundColor: 'transparent',
     borderBottomColor: 'transparent',
     borderTopColor: 'transparent',
   },
-  // input: {
-  // borderColor: 'white',
-  // backgroundColor: 'white',
-  // },
+  input: {
+    borderColor: 'transparent',
+    backgroundColor: 'transparent',
+  },
 });
